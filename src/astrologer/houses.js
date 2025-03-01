90dbfd176b931cab5274b8d85458a955949fb35c
judisegit <judise@gmail.com>
2025-03-02 00:23:17 +0800

    Update houses.js
    add housenum

--- src/astrologer/houses.js
+++ src/astrologer/houses.js
@@ -29,10 +29,18 @@ const houses = (date, position, houseSystem = 'P') => {
     houseSystem // placidus system...
   ).data
 
-  const houseCollection = housesPositions.map((cuspid) => ({ position: degreesToDms(cuspid), sign: zodiacSign(cuspid) }))
+  // 修改這裡，加入宮位編號
+  const houseCollection = housesPositions.map((cuspid, index) => ({
+    houseNumber: index + 1, // 宮位編號從 1 開始
+    position: degreesToDms(cuspid),
+    sign: zodiacSign(cuspid)
+  }))
 
   const axes = {
-    asc: houseCollection[0], dc: houseCollection[6], mc: houseCollection[9], ic: houseCollection[3]
+    asc: houseCollection[0],  // 第1宮 (上升點)
+    dc: houseCollection[6],   // 第7宮 (下降點)
+    mc: houseCollection[9],   // 第10宮 (中天)
+    ic: houseCollection[3]    // 第4宮 (天底)
   }
 
   return {
